f42887f19dc32743eda730051d1c991a0f9cdeed
Ruben Polanco <andr3z.93@gmail.com>
2024-03-11 21:33:07 -0700

    completed all the pre-req middleware

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,48 @@
-require('express-async-errors');
-require('dotenv').config();
 const express = require('express');
+require('express-async-errors');
+const morgan = require('morgan');
+const cors = require('cors');
+const csurf = require('csurf');
+const helmet = require('helmet');
+const cookieParser = require('cookie-parser');
+//check to see which environment we are using
+const { environment } = require('./config');
+const isProduction = environment === 'production';
+
 const app = express();
+//connect the morgan midddleware for logging info about req and res
+app.use(morgan('dev'))
+//Add the cookie-parser middleware for parsing cookies
+app.use(cookieParser());
+// pasing json bodies from request with contect type of app json
+app.use(express.json());
+
+
+//Security Middleware
+if (!isProduction) {
+    //enable cors only in dev
+    app.use(cors());
+}
+
+// helmet helps set a variety of headers to better secure your app
+app.use(
+    helmet.crossOriginResourcePolicy({
+        policy: 'cross-origin'
+    })
+);
+
+// Set the _csrf token and create req.csrfToken method
+app.use(
+    csurf({
+        cookie: {
+            secure: isProduction,
+            sameSite: isProduction && "Lax",
+            httpOnly: true
+        }
+    })
+);
+
+
 
 const port = 8000;
 app.listen(port, () => console.log('Server-1 is listening on port', port));
