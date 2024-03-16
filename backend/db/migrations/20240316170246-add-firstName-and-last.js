c51057e1c186f1cf83aec4666482502f238cc170
Ruben Polanco <andr3z.93@gmail.com>
2024-03-16 10:21:01 -0700

    added first and last name to user model

---
+++ backend/db/migrations/20240316170246-add-firstName-and-last.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.addColumn('Users', 'firstName', Sequelize.STRING, {
+      after: 'id'
+    });
+
+    await queryInterface.addColumn('Users', 'lastName', Sequelize.STRING, {
+      after: 'firstName'
+    })
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.removeColumn('Users', 'lastName');
+
+    await queryInterface.removeColumn('Users', 'firstName');
+  }
+};
