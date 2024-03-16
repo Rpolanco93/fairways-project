6c2d40992cbc4cf9028d3f74f2bd87b0364ed4fb
Ruben Polanco <andr3z.93@gmail.com>
2024-03-15 17:57:19 -0700

    Add User logout backend endpoint

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -42,8 +42,16 @@ router.post(
         user: safeUser
       });
     }
-  );
+);
 
+// Log out
+router.delete(
+    '/',
+    (_req, res) => {
+      res.clearCookie('token');
+      return res.json({ message: 'success' });
+    }
+  );
 
 
 module.exports = router;
