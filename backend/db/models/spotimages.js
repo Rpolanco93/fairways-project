6fd92440c2838d3d528e52c4e3068c97e6a00d7c
Ruben Polanco <andr3z.93@gmail.com>
2024-04-04 20:54:03 -0700

    testing changes to post spots/id/image

--- backend/db/models/spotimages.js
+++ backend/db/models/spotimages.js
@@ -13,6 +13,7 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
       SpotImages.belongsTo(
         models.Spot,
+        { foreignKey: 'spotId' }
       )
     }
   }
