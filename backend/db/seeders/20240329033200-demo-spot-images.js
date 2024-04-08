0c3aac5e49ea89ce09fe20236d66465db2bf96d0
Ruben Polanco <andr3z.93@gmail.com>
2024-04-08 09:28:55 -0700

    updated the down for spot image seed

--- backend/db/seeders/20240329033200-demo-spot-images.js
+++ backend/db/seeders/20240329033200-demo-spot-images.js
@@ -38,7 +38,7 @@ module.exports = {
     options.tableName = 'ReviewImages';
     const Op = Sequelize.Op;
     return queryInterface.bulkDelete(options, {
-      reviewId: { [Op.in]: [1,2,3,4,5] }
+      spotId: { [Op.in]: [1,2,3,4,5] }
     }, {});
   }
 };
