b3d29039bd76446915a159890ca184392b7975cc
Ruben Polanco <andr3z.93@gmail.com>
2024-04-07 21:12:11 -0700

    updated to User  migration file

--- backend/db/migrations/20240314031338-create-user.js
+++ backend/db/migrations/20240314031338-create-user.js
@@ -42,6 +42,6 @@ module.exports = {
   },
   async down(queryInterface, Sequelize) {
     options.tablename = 'Users';
-    return queryInterface.dropTable(options)
+    return queryInterface.dropTable("Users")
   }
 };
