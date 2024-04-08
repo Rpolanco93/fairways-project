0617f121a352f9f6a6e18bb1b9ba4bd31313352f
Ruben Polanco <andr3z.93@gmail.com>
2024-04-08 16:59:42 -0700

    updated error messages

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -4,7 +4,8 @@ const bcrypt = require('bcryptjs');
 const { restoreUser, requireAuth } = require('../../utils/auth.js');
 const { Spot, Review, SpotImages, User, ReviewImages } = require('../../db/models');
 const { Op, Sequelize } = require('sequelize');
-
+const { check } = require('express-validator')
+const { handleValidationErrors } = require('../../utils/validation.js')
 const router = express.Router();
 
 const validateReview = [
