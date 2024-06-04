c766e691ee8ec544a57615b6a1e6f9513fef9f9e
Ruben Polanco <andr3z.93@gmail.com>
2024-06-03 17:09:14 -0700

    completed phase 3

--- frontend/src/components/Navigation/ProfileButton.jsx
+++ frontend/src/components/Navigation/ProfileButton.jsx
@@ -1,45 +1,46 @@
-import { SiAeromexico } from "react-icons/si";
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { useDispatch } from 'react-redux';
-// import { FaUserCircle } from 'react-icons/fa';
+import { FaUserCircle } from 'react-icons/fa';
 import * as sessionActions from '../../store/session';
 
 function ProfileButton({ user }) {
-    const dispatch = useDispatch();
-    const [showMenu, setShowMenu] = useState(false);
-    const ulRef = useRef();
+  const dispatch = useDispatch();
+  const [showMenu, setShowMenu] = useState(false);
+  const ulRef = useRef();
 
-    const toggleMenu = (e) => {
-      e.stopPropagation(); // Keep click from bubbling up to document and triggering closeMenu
-      setShowMenu(!showMenu);
+  const toggleMenu = (e) => {
+    e.stopPropagation(); // Keep click from bubbling up to document and triggering closeMenu
+    // if (!showMenu) setShowMenu(true);
+    setShowMenu(!showMenu);
+  };
+
+  useEffect(() => {
+    if (!showMenu) return;
+
+    const closeMenu = (e) => {
+      if (ulRef.current && !ulRef.current.contains(e.target)) {
+        setShowMenu(false);
+      }
     };
 
-    useEffect(() => {
-      if (!showMenu) return;
+    document.addEventListener('click', closeMenu);
 
-      const closeMenu = (e) => {
-        if (ulRef.current && !ulRef.current.contains(e.target)) {
-            setShowMenu(false);
-      };
+    return () => document.removeEventListener('click', closeMenu);
+  }, [showMenu]);
 
-      document.addEventListener('click', closeMenu);
+  const logout = (e) => {
+    e.preventDefault();
+    dispatch(sessionActions.logout());
+  };
 
-      return () => document.removeEventListener("click", closeMenu);
-    }, [showMenu]);
-
-    const logout = (e) => {
-      e.preventDefault();
-      dispatch(sessionActions.logout());
-    };
-
-    const ulClassName = "profile-dropdown" + (showMenu ? "" : " hidden");
+  const ulClassName = "profile-dropdown" + (showMenu ? "" : " hidden");
 
   return (
     <>
       <button onClick={toggleMenu}>
-        <SiAeromexico />
+        <FaUserCircle />
       </button>
-      <ul className={ulClassName} ref={ulRef}> {/* <-- Attach it here */}
+      <ul className={ulClassName} ref={ulRef}>
         <li>{user.username}</li>
         <li>{user.firstName} {user.lastName}</li>
         <li>{user.email}</li>
