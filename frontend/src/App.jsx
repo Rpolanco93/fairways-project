367edf4311c81bfe0b1188169baf6af1c3ad2221
Ruben Polanco <andr3z.93@gmail.com>
2024-06-06 19:25:01 -0700

    completed phase 4

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom'
 import { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import LoginFormPage from './components/LoginFormPage';
 import SignupFormPage from './components/SignupFormPage';
 import Navigation from './components/Navigation';
 import * as sessionActions from './store/session';
@@ -19,7 +18,7 @@ function Layout() {
   return (
     <>
       <Navigation isLoaded={isLoaded} />
-      <Outlet />
+      {isLoaded && <Outlet />}
     </>
   )
 }
@@ -31,10 +30,6 @@ const router = createBrowserRouter([
       {
         path: '/',
         element: <h1>Welcome!</h1>
-      },
-      {
-        path: '/login',
-        element: <LoginFormPage />
       },
       {
         path: '/signup',
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import { Provider } from 'react-redux';
+import { Modal, ModalProvider } from './context/Modal';
 import configureStore from './store/store';
 import { restoreCSRF, csrfFetch } from './store/csrf';
 import * as sessionActions from './store/session';
@@ -23,8 +24,11 @@ if (process.env.NODE_ENV !== 'production') {
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <ModalProvider>
+      <Provider store={store}>
+        <App />
+        <Modal />
+      </Provider>
+    </ModalProvider>
   </React.StrictMode>
 );
